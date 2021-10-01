e46e5e0109e8a2c0b33e0f57047945d4856931ef
Aastha0111 <aasthapatil0111@gmail.com>
2021-10-02 02:30:02 +0530

    JS Blog

--- script.js
+++ script.js
@@ -1,1 +1,48 @@
 /* Fill your code*/
+document.getElementById('addBlog').addEventListener('click',function(){
+    document.getElementById('popupContact').style.display="block";
+    })
+    document.getElementById('close').addEventListener('click',function(){
+        document.getElementById('popupContact').style.display="none";
+    })
+
+class Blog 
+{
+    constructor(title, detail) 
+    {
+        this.title = title;
+        this.des = detail;
+
+    }
+
+    addTitle() {
+        var title_card = document.createElement('h1');
+        title_card.setAttribute("id","blog-title");
+        console.log(title_card);
+        document.getElementById('card-text').appendChild(title_card);
+        title_card.innerHTML += this.title;
+    }
+
+    addDescription() {
+        var display = document.createElement('p');
+        display.setAttribute("id", "blog-description");
+        console.log(display);
+        document.getElementById('card-text').appendChild(display);
+        display.innerHTML += this.des;
+    }
+}
+
+let post=document.getElementById('post');
+post.addEventListener('click',function(){
+let image=document.createElement('img')
+image.src='./assets/javascript.png';
+image.setAttribute('id','image')
+document.getElementById('card-text').appendChild(image);
+let title=document.getElementById('title').value;
+let detail=document.getElementById('detail').value;
+var card=new Blog(title,detail)
+card.addTitle()
+card.addDescription()
+})
+
+
